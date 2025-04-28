94346ec6eadd619f214afee5714f98299408f740
Sittipu <stpppr2548@gmail.com>
2025-04-28 17:24:24 +0700

    Update backend

--- FullStack/todo-backend/src/index.ts
+++ FullStack/todo-backend/src/index.ts
@@ -1,15 +1,26 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-
-const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { PrismaClient } from "./generated/prisma/index.js";
+import { mainRouter } from "./router/index.routes.ts";
+import { logger } from "hono/logger";
+ 
+const app = new Hono();
+export const db = new PrismaClient();
+ 
+app.use(logger());
+ 
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+ 
+app.route("", mainRouter);
+ 
+serve(
+  {
+    fetch: app.fetch,
+    port: 8000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
--- FullStack/todo-backend/src/router/todo.routes.ts
+++ FullStack/todo-backend/src/router/todo.routes.ts
@@ -1,7 +1,13 @@
 import { Hono } from "hono";
+import { GetTodo, AddTodo, EditTodoName, CompleteTodo, DeleteTodo } from "../controller/todo.controller.ts";
+import { todo } from "node:test";
 
 const todoRouter = new Hono();
 
 todoRouter.get("/test", (c) => c.text("Hello From Todo Router"));
-
+todoRouter.get("/", GetTodo);
+todoRouter.post("/", AddTodo);
+todoRouter.patch("/", EditTodoName);
+todoRouter.patch("/complete", CompleteTodo);
+todoRouter.delete("/:todoId", DeleteTodo)
 export { todoRouter };
